241665ce0cc9a10ef84471758b5dcb8e1bdb592b
mzahidh <muhmmadzaahid1998@gmail.com>
2020-02-10 11:27:16 +0700

    'coba'

---
+++ day1.py
@@ -0,0 +1,11 @@
+pertambahan = 25 + 25
+pengurangan = 25-23
+pembagian = 2/4
+perkalian = 7*4
+modulus = 8%2
+
+print (pertambahan)
+print (pembagian)
+print (perkalian)
+print (pengurangan)
+print (modulus)
